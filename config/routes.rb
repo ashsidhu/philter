10c2d270fc7dbd7cfffd74fa0091813567a0a829
David Lee <ambitiouspear@gmail.com>
2014-10-25 15:59:36 -0700

    Add api routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,12 @@ Rails.application.routes.draw do
   get 'pages/home'
     namespace :api do
       namespace :v1 do
-
+        #products#index - admin dashboard summary
+        #products#show - display product info for reviewer
+        #products/reviews#create - create review info
+        resources :products, only: [:show, :index] do
+          resources :reviews, only: [:create]
+        end
       end
     end
 end
