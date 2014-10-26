db90346b6224287865789f5632812285f6015c5b
David Lee <ambitiouspear@gmail.com>
2014-10-25 17:17:57 -0700

    Initialize clearance and add views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141025213050) do
+ActiveRecord::Schema.define(version: 20141026001457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,5 +47,17 @@ ActiveRecord::Schema.define(version: 20141025213050) do
   end
 
   add_index "reviews", ["product_id"], name: "index_reviews_on_product_id", using: :btree
+
+  create_table "users", force: true do |t|
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.string   "email",                          null: false
+    t.string   "encrypted_password", limit: 128, null: false
+    t.string   "confirmation_token", limit: 128
+    t.string   "remember_token",     limit: 128, null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", using: :btree
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
 
 end
