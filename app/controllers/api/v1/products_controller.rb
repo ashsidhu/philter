5b0a201920a0192bd59a25934e1315b89ffb3f4e
David Lee <ambitiouspear@gmail.com>
2014-10-25 19:23:40 -0700

    Add product api show

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::ProductsController < ApplicationController
   def show
+    @product = Product.find(params[:id])
+    respond_to do |format|
+      format.json { render json: @product }
+    end
   end
 
   def create
