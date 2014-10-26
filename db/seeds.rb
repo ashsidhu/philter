6e2b13be5199ba2150c1d1db7e47feb2732825e6
David Lee <ambitiouspear@gmail.com>
2014-10-25 22:58:35 -0700

    Truncate user_seed_data file and update seeds.rb for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,22 +13,31 @@ categories.each do |category_name|
   Category.find_or_create_by(name: category_name)
 end
 
-developer_token = "71726e638c1ff0d9989da4fee0e2029a8a452ae7bf93edfa3f1eb962e5fed0cd"
-post_names = []
-(0..10).each do |days_ago|
-  product_api_url = "https://api.producthunt.com/v1/posts?access_token=#{developer_token}&days_ago=#{days_ago.to_s}"
-  response = HTTParty.get(product_api_url)
-  response["posts"].each do |post|
-    post_names << post["name"]
-    id = post["id"]
-    name = post["name"]
-    tagline =  post["tagline"]
-    created_at = DateTime.strptime(post["created_at"].split(".")[0],'%Y-%m-%dT%H:%M:%S')
-    screenshot_url = post["screenshot_url"]["850px"].truncate(255)
-    begin
-      Product.create(id: id, name: name, tagline: tagline, created_at: created_at, screenshot_url: screenshot_url, approved: 1)
-    rescue => e
-      puts "#{e} \n"
+if Product.count < 20
+  developer_token = "71726e638c1ff0d9989da4fee0e2029a8a452ae7bf93edfa3f1eb962e5fed0cd"
+  post_names = []
+  (0..10).each do |days_ago|
+    product_api_url = "https://api.producthunt.com/v1/posts?access_token=#{developer_token}&days_ago=#{days_ago.to_s}"
+    response = HTTParty.get(product_api_url)
+    response["posts"].each do |post|
+      post_names << post["name"]
+      id = post["id"]
+      name = post["name"]
+      tagline =  post["tagline"]
+      created_at = DateTime.strptime(post["created_at"].split(".")[0],'%Y-%m-%dT%H:%M:%S')
+      screenshot_url = post["screenshot_url"]["850px"].truncate(255)
+      begin
+        Product.create(id: id, name: name, tagline: tagline, created_at: created_at, screenshot_url: screenshot_url, approved: 1)
+      rescue => e
+        puts "#{e} \n"
+      end
     end
   end
 end
+
+open("lib/assets/user_seed_data.txt").each do |line|
+  line = line.split(",")
+  User.create(user_name: line[0], upvotes_count: line[1], name: line[2], score: line[3], followers_count: line[4], comments_count: line[5], password: "password", email: "#{line[0]}@producthunt.com")
+end
+
+
