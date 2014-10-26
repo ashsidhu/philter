87636b83a5a2434e7a6720d4b4aa819bf807bf56
Richard Yang <elyrly@gmail.com>
2014-10-25 18:52:47 -0700

    new product

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  get 'products/index'
-
   root to: "pages#home"
-  get 'products/new'
 
-  get 'products/create'
+  post 'products/new' =>'products#new'
 
   get 'pages/home'
-  resources :products, only: [:index]
     namespace :api do
       namespace :v1 do
         #products#index - admin dashboard summary
